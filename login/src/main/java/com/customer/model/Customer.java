09db4de1292982013c43f8f6d065519b4a66bdd3
Sumit Kamboj <sumitkamboj88@gmail.com>
2015-10-05 13:45:04 +0530

    inital commit

---
+++ login/src/main/java/com/customer/model/Customer.java
@@ -0,0 +1,41 @@
+package com.customer.model;
+import java.util.Date;
+public class Customer implements java.io.Serializable{
+	private Long customerId;
+	private Long password;
+	private String name;
+	private String address;
+	private Date createdDate;
+	
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public Long getPassword() {
+		return password;
+	}
+	public void setPassword(Long password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+}
